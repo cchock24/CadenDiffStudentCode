973621649e9ca0d0ad545ba4d4d90810c52bbdc2
Caden <caden.chock@menloschool.org>
2025-02-28 00:56:23 -0800

    .

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -15,9 +15,43 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
+        int length1 = doc1.length();
+        int length2 = doc2.length();
+        int[][] saved = new int[length1][length2];
 
-        // TODO Complete this function to return the length of the longest shared substring.
+        return check(doc1,doc2,0,0,saved);
+    }
 
-        return 0;
+    public static int check(String doc1, String doc2, int index1, int index2, int[][] saved){
+       int[] length = {0,0,0};
+        // Base Case if either word reaches end
+        if(index1 >= doc1.length()){
+            return 0;
+        }
+        if(index2 >= doc2.length()){
+            return 0;
+        }
+        // Memoization Step
+        if(saved[index1][index2] != 0){
+            return saved[index1][index2];
+        }
+        // If letter matches include
+        // Otherwise remove from doc1 and remove from doc2
+        if(doc1.charAt(index1) == doc2.charAt(index2)){
+            length[0] += check(doc1,doc2,index1+1,index2+1,saved) +1;
+        }
+        else{
+            length[1] += check(doc1,doc2,index1+1,index2,saved);
+            length[2] += check(doc1,doc2,index1,index2+1,saved);
+        }
+        // Get the biggest length from all the paths
+        int biggest = 0;
+        for(int i = 0; i < length.length; i++){
+            if(length[i] > biggest){
+                biggest = length[i];
+            }
+        }
+        saved[index1][index2] = biggest;
+        return biggest;
     }
 }
